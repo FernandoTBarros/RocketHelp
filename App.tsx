788e771762789a366f30cb480616f181177fb670
Fernando T. Barros <fernando@deltasi.com.br>
2022-07-19 02:49:05 -0300

    Finishing class 3

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-import { useFonts, Roboto_400Regular, Roboto_700Bold  } from '@expo-google-fonts/roboto';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { THEME } from './src/styles/theme';
 
-import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
 
 export default function App() {
 	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -13,8 +13,8 @@ export default function App() {
 			<StatusBar
 				barStyle="light-content"
 				backgroundColor="transparent"
-				translucent/>
-			{fontsLoaded ? <SignIn /> : <Loading/> }
+				translucent />
+			{fontsLoaded ? <Routes /> : <Loading />}
 		</NativeBaseProvider>
 	);
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import { useNavigation } from '@react-navigation/native';
+import { Heading, HStack, IconButton, StyledProps, useTheme } from 'native-base';
+import { CaretLeft } from 'phosphor-react-native';
+
+type Props = StyledProps & {
+	title: string;
+}
+
+export function Header({ title, ...rest }: Props) {
+	const navigation = useNavigation();
+
+	const { colors } = useTheme();
+
+	function handleGoBack() {
+		navigation.goBack();
+	}
+	return (
+		<HStack
+			w="full"
+			justifyContent="space-between"
+			alignItems="center"
+			bg="gray.600"
+			pb={6}
+			pt={12}
+			{...rest}
+		>
+			<IconButton onPress={handleGoBack} icon={<CaretLeft size={24} color={colors.gray[200]} />} />
+
+			<Heading color="gray.100" textAlign="center" flex={1} fontSize="lg" ml={-6}>
+				{title}
+			</Heading>
+
+
+		</HStack>
+	);
+}
